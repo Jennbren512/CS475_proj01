f5a97fc7dea0f3fb9599e378a758bd98ed560f3e
Brenna Jennings <146241617+Jennbren512@users.noreply.github.com>
2025-04-16 14:59:43 +0000

    new

--- proj01.cpp
+++ proj01.cpp
@@ -4,59 +4,62 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
-#include <fstream>
 
 #ifndef F_PI
 #define F_PI (float)M_PI
 #endif
 
-const float GMIN = 10.0, GMAX = 20.0;
-const float HMIN = 20.0, HMAX = 30.0;
-const float DMIN = 10.0, DMAX = 20.0;
-const float VMIN = 20.0, VMAX = 30.0;
-const float THMIN = 70.0, THMAX = 80.0;
-const float GRAVITY = -9.8;
-const float TOL = 5.0;
+// print debugging messages?
+#ifndef DEBUG
+#define DEBUG false
+#endif
 
-const int NUMTRIES = 100;
+// how many tries to discover the maximum performance:
+#ifndef NUMTRIES
+#define NUMTRIES 10
+#endif
 
-float Ranf(float low, float high)
-{
+// ranges for the random numbers:
+const float GMIN = 10.0;   // ground distance in meters
+const float GMAX = 20.0;   // ground distance in meters
+const float HMIN = 20.0;   // cliff height in meters
+const float HMAX = 30.0;   // cliff height in meters
+const float DMIN = 10.0;   // distance to castle in meters
+const float DMAX = 20.0;   // distance to castle in meters
+const float VMIN = 20.0;   // initial cannonball velocity in m/s
+const float VMAX = 30.0;   // initial cannonball velocity in m/s
+const float THMIN = 70.0;  // cannonball launch angle in degrees
+const float THMAX = 80.0;  // cannonball launch angle in degrees
+
+const float GRAVITY = -9.8;    // acceleration due to gravity in m/s^2
+const float TOL = 5.0;         // tolerance in meters for a hit
+
+// function prototypes:
+float Ranf(float, float);
+void TimeOfDaySeed();
+
+// degrees-to-radians:
+inline float Radians(float degrees) {
+    return (F_PI / 180.f) * degrees;
+}
+
+// random float in range:
+float Ranf(float low, float high) {
     float r = (float)rand();
     float t = r / (float)RAND_MAX;
     return low + t * (high - low);
 }
 
-int Ranf(int ilow, int ihigh)
-{
-    float low = (float)ilow;
-    float high = ceil((float)ihigh);
-    return (int)Ranf(low, high);
-}
-
-void TimeOfDaySeed()
-{
+// call this to use different random seed each run:
+void TimeOfDaySeed() {
     time_t now;
     time(&now);
-    struct tm n = *localtime(&now);
-    struct tm jan01 = *localtime(&now);
-    jan01.tm_mon = 0;
-    jan01.tm_mday = 1;
-    jan01.tm_hour = 0;
-    jan01.tm_min = 0;
-    jan01.tm_sec = 0;
-    double seconds = difftime(now, mktime(&jan01));
-    unsigned int seed = (unsigned int)(1000. * seconds);
-    srand(seed);
+    struct tm *ptm = localtime(&now);
+    srand(1000 * (60 * ptm->tm_min + ptm->tm_sec));
 }
 
-inline float Radians(float degrees)
-{
-    return (F_PI / 180.f) * degrees;
-}
-
-int main()
-{
+// main program:
+int main() {
 #ifndef _OPENMP
     fprintf(stderr, "No OpenMP support!\n");
     return 1;
@@ -65,102 +68,94 @@ int main()
     TimeOfDaySeed();
 
     int threadCounts[] = {1, 2, 4, 6, 8};
-    int trialCounts[] = {1000, 10000, 50000, 100000, 200000, 300000, 400000, 500000};
+    int trialCounts[] = {100, 1000, 10000, 50000, 100000};
 
-    std::ofstream csvFile("simulation_results.csv");
-    csvFile << "Threads,Trials,MegaTrialsPerSecond\n";
+    // CSV header for Excel import:
+    printf("Threads,Trials,MegaTrialsPerSec\n");
 
-    for (int t = 0; t < sizeof(threadCounts) / sizeof(int); t++)
-    {
-        int NUMT = threadCounts[t];
-        omp_set_num_threads(NUMT);
+    // loop through each thread and trial combination:
+    for (int t = 0; t < sizeof(threadCounts)/sizeof(int); t++) {
+        for (int tr = 0; tr < sizeof(trialCounts)/sizeof(int); tr++) {
+            int NUMT = threadCounts[t];
+            int NUMTRIALS = trialCounts[tr];
+            omp_set_num_threads(NUMT);
 
-        for (int tc = 0; tc < sizeof(trialCounts) / sizeof(int); tc++)
-        {
-            int NUMTRIALS = trialCounts[tc];
+            float *vs = new float[NUMTRIALS];
+            float *ths = new float[NUMTRIALS];
+            float *gs = new float[NUMTRIALS];
+            float *hs = new float[NUMTRIALS];
+            float *ds = new float[NUMTRIALS];
+
+            // fill arrays with randomized inputs:
+            for (int i = 0; i < NUMTRIALS; i++) {
+                vs[i] = Ranf(VMIN, VMAX);
+                ths[i] = Ranf(THMIN, THMAX);
+                gs[i] = Ranf(GMIN, GMAX);
+                hs[i] = Ranf(HMIN, HMAX);
+                ds[i] = Ranf(DMIN, DMAX);
+            }
+
             double maxPerformance = 0.;
             int numHits = 0;
 
-            for (int tries = 0; tries < NUMTRIES; tries++)
-            {
-                // Re-randomize input arrays each timing try
-                float* vs = new float[NUMTRIALS];
-                float* ths = new float[NUMTRIALS];
-                float* gs = new float[NUMTRIALS];
-                float* hs = new float[NUMTRIALS];
-                float* ds = new float[NUMTRIALS];
-
-                for (int n = 0; n < NUMTRIALS; n++)
-                {
-                    vs[n] = Ranf(VMIN, VMAX);
-                    ths[n] = Ranf(THMIN, THMAX);
-                    gs[n] = Ranf(GMIN, GMAX);
-                    hs[n] = Ranf(HMIN, HMAX);
-                    ds[n] = Ranf(DMIN, DMAX);
-                }
-
-                int hits = 0;
+            // NUMTRIES for max performance
+            for (int tries = 0; tries < NUMTRIES; tries++) {
                 double time0 = omp_get_wtime();
+                int hits = 0;
 
-                #pragma omp parallel for reduction(+:hits)
-                for (int n = 0; n < NUMTRIALS; n++)
-                {
-                    float v = vs[n];
-                    float thr = Radians(ths[n]);
+                #pragma omp parallel for default(none) shared(vs, ths, gs, hs, ds, NUMTRIALS) reduction(+:hits)
+                for (int i = 0; i < NUMTRIALS; i++) {
+                    float v = vs[i];
+                    float thr = Radians(ths[i]);
                     float vx = v * cos(thr);
                     float vy = v * sin(thr);
-                    float g = gs[n];
-                    float h = hs[n];
-                    float d = ds[n];
+                    float g = gs[i];
+                    float h = hs[i];
+                    float d = ds[i];
 
-                    float t_flight = -vy / (0.5 * GRAVITY);
+                    float t_flight = (-2. * vy) / GRAVITY;
                     float x = vx * t_flight;
-                    if (x <= g) continue;
+                    if (x <= g) continue; // didn't reach cliff
 
                     float t_cliff = g / vx;
-                    float y_cliff = vy * t_cliff + 0.5 * GRAVITY * t_cliff * t_cliff;
-                    if (y_cliff <= h) continue;
+                    float y = vy * t_cliff + 0.5 * GRAVITY * t_cliff * t_cliff;
+                    if (y <= h) continue; // hit cliff face
 
                     float A = 0.5 * GRAVITY;
                     float B = vy;
                     float C = -h;
-                    float disc = B * B - 4.f * A * C;
-                    if (disc < 0.) continue;
+                    float discriminant = B * B - 4 * A * C;
+                    if (discriminant < 0) continue; // shouldn't happen
 
-                    float t1 = (-B + sqrtf(disc)) / (2.f * A);
-                    float t2 = (-B - sqrtf(disc)) / (2.f * A);
-                    float tmax = fmaxf(t1, t2);
-                    float upperDist = vx * tmax - g;
+                    float sqrtDisc = sqrtf(discriminant);
+                    float t1 = (-B + sqrtDisc) / (2. * A);
+                    float t2 = (-B - sqrtDisc) / (2. * A);
+                    float t_upper = t1 > t2 ? t1 : t2;
 
-                    if (fabs(upperDist - d) <= TOL)
-                        hits++;
+                    float x_land = vx * t_upper - g;
+                    if (fabs(x_land - d) <= TOL) hits++;
                 }
 
                 double time1 = omp_get_wtime();
                 double megaTrialsPerSecond = (double)NUMTRIALS / (time1 - time0) / 1e6;
-
                 if (megaTrialsPerSecond > maxPerformance)
-                {
                     maxPerformance = megaTrialsPerSecond;
-                    numHits = hits;
-                }
 
-                delete[] vs;
-                delete[] ths;
-                delete[] gs;
-                delete[] hs;
-                delete[] ds;
+                numHits = hits;
             }
 
-            float probability = (float)numHits / (float)NUMTRIALS;
+            float probability = (float)numHits / (float)(NUMTRIALS);
 
-            fprintf(stderr, "%2d threads : %8d trials ; probability = %6.2f%% ; megatrials/sec = %6.2lf\n",
-                NUMT, NUMTRIALS, 100. * probability, maxPerformance);
+            // output for Excel
+            printf("%d,%d,%6.2lf\n", NUMT, NUMTRIALS, maxPerformance);
 
-            csvFile << NUMT << "," << NUMTRIALS << "," << maxPerformance << "\n";
+            delete[] vs;
+            delete[] ths;
+            delete[] gs;
+            delete[] hs;
+            delete[] ds;
         }
     }
 
-    csvFile.close();
     return 0;
 }
